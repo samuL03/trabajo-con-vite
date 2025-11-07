f85c5c6de9ca3cf8fb3ae0d4931397eaa9d45fb9
Samuel-Rendon-Loaiza <samuelrendonloaiza7@gmail.com>
2025-11-06 19:50:03 -0500

    feat: componente Educacion con formación complementaria

---
+++ exp_app/src/educacion.jsx
@@ -0,0 +1,15 @@
+function Educacion() {
+  return (
+    <section>
+      <h3>Educación y Formación</h3>
+      <ul>
+        <li>Tecnólogo en Análisis y Desarrollo de Software – SENA (2025)</li>
+        <li>Curso React Básico – Platzi (2024)</li>
+        <li>Taller de Diseño Web – Google Activate (2023)</li>
+        <li>Seminario: Buenas prácticas en Git y GitHub – SENA (2023)</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Educacion;
