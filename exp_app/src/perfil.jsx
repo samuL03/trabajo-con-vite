f61f48eb5c7110c2db21fae4251fe34651584973
Samuel-Rendon-Loaiza <samuelrendonloaiza7@gmail.com>
2025-11-06 19:42:30 -0500

    feat: componente Perfil con descripción personalizada

---
+++ exp_app/src/perfil.jsx
@@ -0,0 +1,20 @@
+function Perfil() {
+  return (
+    <section>
+      <h3>Perfil Profesional</h3>
+      <p>
+        Soy un desarrollador en formación con interés en la creación de interfaces web intuitivas y funcionales. 
+        Me caracterizo por el trabajo en equipo, la responsabilidad y la constante disposición al aprendizaje.
+      </p>
+      <h4>Habilidades</h4>
+      <ul>
+        <li>HTML, CSS, JavaScript, React</li>
+        <li>Trabajo en equipo</li>
+        <li>Comunicación efectiva</li>
+        <li>Resolución de problemas</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Perfil;
