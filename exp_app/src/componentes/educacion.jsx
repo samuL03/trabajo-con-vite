27736bf252f50ac58aa983ab6444bad9c170ebee
Samuel-Rendon-Loaiza <samuelrendonloaiza7@gmail.com>
2025-11-10 07:42:49 -0500

    feat: componente Educacion dinamico con formación complementaria

--- exp_app/src/componentes/educacion.jsx
+++ exp_app/src/componentes/educacion.jsx
@@ -1,38 +1,27 @@
-import React from "react";
-
 function Educacion() {
-  const educacion = [
-    {
-      institucion: "I.E Alberto Diaz Muñoz",
-      curso: "Bachiller Académico",
-      año: 2024,
-    },
-    {
-      institucion: "SENA - Centro de Tecnología de la Manufactura Avanzada (CTMA)",
-      curso: "Tecnólogo en Análisis y Desarrollo de Software (ADSO)",
-      año: "2025 - 2027",
-    },
-    {
-      institucion: "SENA",
-      curso: "Inglés Nivel 1",
-      año: 2025,
-    },
-    {
-      institucion: "SENA",
-      curso: "Inglés Nivel 2",
-      año: 2025,
-    },
+  const estudios = [
+    { institucion: "SENA", curso: "Tecnólogo en Análisis y Desarrollo de Software", año: 2025 },
+    { institucion: "Google", curso: "Curso de React Avanzado", año: 2024 },
+    { institucion: "Platzi", curso: "Fundamentos de JavaScript", año: 2023 },
+    { institucion: "Udemy", curso: "Diseño Web Responsivo", año: 2023 },
+    { institucion: "Coursera", curso: "Bases de Datos con SQL", año: 2024 },
+    { institucion: "SENA", curso: "Programación en Node.js", año: 2025 },
+    { institucion: "Alura", curso: "React Hooks y Context API", año: 2024 },
+    { institucion: "SENA", curso: "Git y GitHub Básico", año: 2023 },
+    { institucion: "FreeCodeCamp", curso: "Responsive Web Design", año: 2022 },
+    { institucion: "Microsoft Learn", curso: "Introducción a Azure", año: 2024 }
   ];
 
   return (
     <div>
-      <ul style={{ listStyleType: "square", lineHeight: "1.8" }}>
-        {educacion.map((edu, index) => (
-          <li key={index}>
-            <strong>{edu.curso}</strong> - {edu.institucion} ({edu.año})
-          </li>
-        ))}
-      </ul>
+      <h2>Educación</h2>
+      {estudios.map((edu, index) => (
+        <div key={index}>
+          <h3>{edu.curso}</h3>
+          <p><strong>Institución:</strong> {edu.institucion}</p>
+          <p><strong>Año:</strong> {edu.año}</p>
+        </div>
+      ))}
     </div>
   );
 }
